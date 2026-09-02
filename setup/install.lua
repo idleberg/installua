8cda54596d1fd0dd73a2e84bceabad2d49eaa229
Jan T. Sott <git@idleberg.com>
2026-09-02 23:47:56 +0200

    feat: add installer script

---
+++ setup/install.lua
@@ -0,0 +1,45 @@
+local EnVar = plugin "EnVar"
+
+local APP <const> = "installua"
+local VERSION <const> = param("VERSION", "dev")
+local BINARY <const> = param("BINARY", "../target/release/installua.exe")
+
+attributes {
+  name = APP,
+  outFile = APP .. "-" .. VERSION .. "-setup.exe",
+  unicode = true,
+  compressor = "lzma",
+  requestExecutionLevel = "user",
+}
+
+installer {
+  installDir = LOCALAPPDATA .. "/Programs/" .. APP,
+
+  page.directory {},
+  page.instFiles {},
+
+  onInit(function()
+    -- Rust's tier-1 Windows floor is Windows 10; 7/8.1 are tier 3 and the
+    -- binary will not load there.
+    if getWinVer("MAJOR") < 10 then
+      messageBox {
+        text = APP .. " requires Windows 10 or later.",
+        buttons = "OK",
+        icon = "STOP",
+      }
+      abort()
+    end
+  end),
+
+  section("Core", function()
+    setOutPath(INSTDIR)
+    file(BINARY)
+
+    EnVar.setHKCU()
+    local code = EnVar.addValue("PATH", INSTDIR)
+
+    if code ~= 0 then
+      detailPrint("could not add " .. INSTDIR .. " to PATH (code " .. code .. ")")
+    end
+  end),
+}
