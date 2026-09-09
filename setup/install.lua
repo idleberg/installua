3de609e769bfb953ff8670fe4f02af4cc9c24d4b
Jan T. Sott <git@idleberg.com>
2026-09-09 23:38:20 +0200

    chore: rename installer

--- setup/install.lua
+++ setup/install.lua
@@ -6,7 +6,7 @@ local BINARY <const> = param("BINARY", "../target/release/installua.exe")
 
 attributes {
   name = APP,
-  outFile = APP .. "-" .. VERSION .. "-windows-setup.exe",
+  outFile = APP .. "-" .. VERSION .. "-windows-x64.exe",
   unicode = true,
   compressor = "lzma",
   requestExecutionLevel = "user",
